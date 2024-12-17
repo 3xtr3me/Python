2770f369a426ed47119a0e4d0f65d24340da533e
Syed Khaild Tipu Razvi <110707546+SKTR13055@users.noreply.github.com>
2024-12-17 19:54:45 +0530

    Challenge Reverse The Number

---
+++ Practice_Questions/Reverse_Number.py
@@ -0,0 +1,9 @@
+#Challenge:
+#Write a program to reverse a number entered by the user.
+#Example:
+#Input: 1234
+#Output: 4321
+
+Number = int(input('Enter a number'))
+Reversed_Number = int(str(Number)[::-1])
+print(Reversed_Number)
