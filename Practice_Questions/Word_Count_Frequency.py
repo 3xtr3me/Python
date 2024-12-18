11fadb218e742b6655bb838e5a0b34f6b7330cf6
Syed Khaild Tipu Razvi <110707546+SKTR13055@users.noreply.github.com>
2024-12-18 21:18:50 +0530

    Challenge Word_Count_Frequency

---
+++ Practice_Questions/Word_Count_Frequency.py
@@ -0,0 +1,9 @@
+#Writing the word count frequency function
+#Taking the sentence from the user
+sentence = input('Enter a Sentence: ').split()
+word_count = {}
+for word in sentence: #checking the word in split sentence
+    word_count[word] = word_count.get(word, 0) + 1
+#If it’s not in the dictionary, it's added with a value of 1.
+#If it’s already in the dictionary, its value is incremented by 1.
+print(word_count)
